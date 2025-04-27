d8ee8b87c70f276d7570280921dd5efd27a67068
Saturnn <adam.vanston@gmail.com>
2025-04-27 18:22:12 +0200

    edit - modif de noms de packages et dossiers

--- src/main/java/com/example/walibixgr2eq6/Controller/HelloController.java
+++ src/main/java/com/example/walibixgr2eq6/Controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.walibixgr2eq6.controller;
+package com.example.walibixgr2eq6.Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,11 @@
 module com.example.walibixgr2eq6 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.walibixgr2eq6 to javafx.fxml;
     exports com.example.walibixgr2eq6;
-    exports com.example.walibixgr2eq6.controller;
-    opens com.example.walibixgr2eq6.controller to javafx.fxml;
+    exports com.example.walibixgr2eq6.Controller;
+    opens com.example.walibixgr2eq6.Controller to javafx.fxml;
 }
