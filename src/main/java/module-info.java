30aa381a7154e8f02a8f0502bcaf11a43d6af304
ineshaller <ines.haller@edu.ece.fr>
2025-05-09 23:08:06 +0200

    push car bug chez Amélie alors que même code

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,14 @@ module com.example.walibixgr2eq6 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.desktop;
 
 
     opens com.example.walibixgr2eq6 to javafx.fxml;
     exports com.example.walibixgr2eq6;
     exports com.example.walibixgr2eq6.Controller;
     opens com.example.walibixgr2eq6.Controller to javafx.fxml;
+    opens com.example.walibixgr2eq6.Model to javafx.base;
+    exports com.example.walibixgr2eq6.Model;
+
 }
